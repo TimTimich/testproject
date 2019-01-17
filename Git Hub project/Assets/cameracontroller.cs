b253e4337c113d390cc960af3a8bdfedb530d58e
TimTimich <31429414+TimTimich@users.noreply.github.com>
2019-01-16 17:14:37 -0800

    Basic setup

---
+++ Git Hub project/Assets/cameracontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour {
+	public float sens= 4f;
+	public float rotX;
+	private float rotY;
+	public GameObject eyes;
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rotX = Input.GetAxis ("Mouse X") * sens;
+		rotY -= Input.GetAxis ("Mouse Y") * sens;
+		rotY = Mathf.Clamp (rotY, -60f, 60f);
+		transform.Rotate (0, rotX, 0);
+		eyes.transform.localRotation = Quaternion.Euler(rotY, 0, 0);
+		//eyes.transform.Rotate(rotY, 0, 0);
+
+		if(Input.GetKey(KeyCode.Tab))
+			{
+				Cursor.lockState = CursorLockMode.Locked;
+			}
+				
+	}
+}
---
+++ Git Hub project/Assets/movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+	public float speed = 70f;
+	[Range(1,10)]
+	public float jump;
+	public float fallm=2.5f;
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			Debug.Log ("pressed");
+			rb.velocity = Vector3.up * jump;
+		}
+
+		float translation = Input.GetAxisRaw ("Vertical") * speed * Time.deltaTime;
+		float strafe = Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime;
+		transform.Translate (strafe, 0, translation);
+	}
+}
